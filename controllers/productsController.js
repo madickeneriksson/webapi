4c655687ae8569e7901d6f8dd8a84e4cbfea3743
Madicken Eriksson <conlerc@live.se>
2022-12-12 14:27:18 +0100

    api

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -7,26 +7,38 @@ controller.param("articleNumber", (req, res, next, articleNumber ) => {
  req.product = products.find(x => x.articleNumber == articleNumber)
     next()
 })
+
 controller.param("tag", (req, res, next, tag ) => {
-    req.product = products.filter(x => x.tag== tag)
+    req.products = products.filter(x => x.tag == tag)
        next()
    })
 
 controller.route('/details/:articleNumber').get((req, res) => {
     if(req.product != undefined)
-    res.status(200).json(req.products)
+    res.status(200).json(req.product)
     else
     res.status(404).json()
 })
 
 controller.route('/:tag').get((req, res) => {
-    if(req.product != undefined)
+    if(req.products != undefined)
     res.status(200).json(req.products)
     else
     res.status(404).json() 
 })
+
+controller.route('/:tag/:take').get((req, res) => {
+    let list = []
+
+    for (let i = 0; i < Number(req.params.take); i++)
+        list.push(req.products[i])
+
+    res.status(200).json(list)
+})
+
 controller.route('/').get((req, res) => {
     res.status(200).json(products)
+
 })
 
 module.exports = controller
